87e3e91aeefce7efaef33eafc6881347f909eafa
ShayKalisher1234 <140321298+ShayKalisher1234@users.noreply.github.com>
2023-07-25 12:18:48 +0300

    Add files via upload

--- ExampleCommand.java
+++ ExampleCommand.java
@@ -4,44 +4,32 @@
 
 package frc.robot.commands;
 
-import java.util.function.DoubleSupplier;
-
+import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.turnMotor;
 
-public class Speed extends CommandBase {
-    /** Creates a new Speed. */
-  private DoubleSupplier speed;
-  private turnMotor motor;
+/** An example command that uses an example subsystem. */
+public class ExampleCommand extends CommandBase {
   /**
-   * @param motor motor
-   * @param speed speed
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
    */
-  public Speed(turnMotor motor , DoubleSupplier speed) {
-    this.speed = speed;
-    this.motor = motor;
-    addRequirements(motor);
+  public ExampleCommand(ExampleSubsystem subsystem) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(subsystem);
   }
-  
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {}
 
   // Called every time the scheduler runs while the command is scheduled.
-  /* (non-Javadoc)
-   * @see edu.wpi.first.wpilibj2.command.Command#execute()
-   */
   @Override
-  public void execute() {
-    motor.motorspeed(speed);
-  }
+  public void execute() {}
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {
-    motor.motorspeed(()-> 0); 
-  }
+  public void end(boolean interrupted) {}
 
   // Returns true when the command should end.
   @Override
